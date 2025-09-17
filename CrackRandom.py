4662594d160bc0c1620ed98b4d5a0c718d7aedbf
wuyan1345 <wuyan_1345@163.com>
2025-09-17 11:52:53 +0800

    2.0 Version

--- CrackRandom.py
+++ CrackRandom.py
@@ -1,7 +1,9 @@
 from sage.all import *
 import logging
 from colorama import Fore, Style, init
-from random import Random, getrandbits
+import random
+import os
+import hashlib
 
 # 初始化 colorama
 init(autoreset=True)
@@ -38,9 +40,12 @@ class CrackRandom:
         """
         Choose loglevel in ["DEBUG","INFO","WARNING","ERROR"].
         """
+        self.T = 19968
         self.L = []
         self.R = []
+        self.blocks = []
         self.bits = []
+        self.state = []
         self.totalbitsize = 0
         self.logger = self._setLog(loglevel)
         self.logger.debug("Init done.")
@@ -55,13 +60,13 @@ class CrackRandom:
         logger.addHandler(handler)
         return logger
     
-    def _checkSufficient(self) -> bool:
-        if self.totalbitsize < 19968:
-            self.logger.info(f"The bits is not enough to crack MT19937. Now got {self.totalbitsize} bits.")
-            return False
-        return True
+    def _checkSufficient(self):
+        matrix_rank = self.L.rank()
+        self.logger.info(f"Matrix rank is {matrix_rank}.")
+        if matrix_rank < 19937:
+            self.logger.warning("Maybe there will be multiple solutions and fail to predict. Try to add more bits.")
     
-    def _constructRow(self, RNG:Random) -> list:
+    def _constructRow(self, RNG:random.Random) -> list:
         row = []
         for bitsize,bitvalue,known in self.bits:
             if known:
@@ -70,8 +75,20 @@ class CrackRandom:
                 RNG.getrandbits(bitsize)
         return row
     
+    def _constructMatrixBlock(self, start:int, end:int) -> Matrix:
+        block = []
+        for i in range(start,end):      # You can use tqdm.trange() to see the exact time.
+            state = [0]*624
+            temp = "0"*i + "1"*1 + "0"*(self.T-1-i)
+            for j in range(624):
+                state[j] = int(temp[32*j:32*j+32], 2)
+            RNG = random.Random()
+            RNG.setstate((3,tuple(state+[624]),None))
+            block.append(self._constructRow(RNG))
+        return Matrix(GF(2), block)
+    
     def _checkState(self, state:list) -> bool:
-        RNG = Random()
+        RNG = random.Random()
         RNG.setstate((3,tuple(state+[624]),None))
         for idx,value in enumerate(self.bits):
             bitsize,bitvalue,known = value
@@ -85,8 +102,21 @@ class CrackRandom:
             else:
                 self.logger.debug(f"No.{idx}: UnknownBit Bitsize({bitsize}) PredictValue({predict})")
                 RNG.getrandbits(bitvalue)
-        return True                
-
+        return True
+    
+    def _findMinPeriod(self, lst:list) -> int:
+        n = len(lst)
+        for p in range(1, n + 1):
+            if all(lst[i] == lst[i % p] for i in range(n)):
+                return p
+        return n
+    
+    def _mySeed(self, key:list) -> int:
+        mySeed = 0
+        for i in range(len(key)-1,-1,-1):
+            mySeed = mySeed << 32
+            mySeed += key[i]-i
+        return mySeed
 
     def uploadValues(self, bitsize:int, bitvalue:int, known:bool=True):
         """
@@ -101,81 +131,223 @@ class CrackRandom:
             self.R += [int(i) for i in bin(bitvalue)[2:].zfill(bitsize)]
             self.logger.debug(f"Already got {self.totalbitsize} bits.")
 
-    def randomPredict(self) -> Random:
+    def randomPredict(self) -> random.Random:
         """
         Bomb!!!
         """
-        if self._checkSufficient():
-            self.logger.info("Start constructing the matrix. It will take a few minutes.")
-            # divide into two parts to avoid storing too much data in memory. (Will be killed in 8GB memory if not to do so)
-            for i in range(19968):        # You can use tqdm.trange() to see the exact time.
-                state = [0]*624
-                temp = "0"*i + "1"*1 + "0"*(19968-1-i)
-                for j in range(624):
-                    state[j] = int(temp[32*j:32*j+32], 2)
-                RNG = Random()
-                RNG.setstate((3,tuple(state+[624]),None))
-                self.L.append(self._constructRow(RNG))
-                if i==9983:
-                    L1 = Matrix(GF(2), self.L)
-                    self.L.clear()
-                    self.logger.info("Half done.")
-            L2 = Matrix(GF(2), self.L)
-            L = L1.stack(L2)
-            R = vector(GF(2), self.R)
-            self.logger.info("Matrix init done.")
-            matrix_rank = L.rank()
-            self.logger.debug(f"Matrix rank is {matrix_rank}.")
-            if matrix_rank < 19937:
-                self.logger.warning("Maybe there will be multiple solutions and fail to predict. Try to add more bits.")
-            self.logger.info("Start solving...")
-            try:
-                s = L.solve_left(R)
-            except:
-                self.logger.error("Can't find the solution. Check your data.")
-                return None
-            init = "".join(list(map(str,s)))
-            state = []
-            for i in range(624):
-                state.append(int(init[32*i:32*i+32],2))
-            if self._checkState(state):
-                self.logger.info("Successfully solved with check.")
-                RNG = Random()
-                RNG.setstate((3,tuple(state+[624]),None))
-                return RNG
-            else:
-                self.logger.error("Unsuccessfully solved. Unable to pass the check.")
-        return None
+        self.logger.info("Start constructing the matrix. It will take a few minutes.")
+        # divide into several parts to avoid storing too much data in memory. (Will be killed in 8GB memory if not to do so)
+        num = 4
+        chunk_size = self.T//num if self.T %num==0 else self.T//num + 1
+        indices = []
+        for i in range(num):
+            start = i * chunk_size
+            end = min((i + 1) * chunk_size, self.T)
+            if start < self.T:
+                indices.append((start, end))
+
+        self.blocks = []
+        for i,(start,end) in enumerate(indices):
+            self.logger.info(f"Constructing block {i+1}/{num}...")
+            self.blocks.append(self._constructMatrixBlock(start, end))
         
+        self.L = self.blocks[0]
+        for i in range(1, len(self.blocks)):
+            self.L = self.L.stack(self.blocks[i])
+
+        R = vector(GF(2), self.R)
+        self.logger.info("Matrix init done.")
+        self._checkSufficient()
+        self.logger.info("Start solving...")
+        try:
+            s = self.L.solve_left(R)
+        except:
+            self.logger.error("Can't find the solution. Check your data.")
+            return None
+        init = "".join(list(map(str,s)))
+        state = []
+        for i in range(624):
+            state.append(int(init[32*i:32*i+32],2))
+        if self._checkState(state):
+            self.logger.info("Successfully solved with check.")
+            self.state = state[:]
+            RNG = random.Random()
+            RNG.setstate((3,tuple(self.state+[624]),None))
+            return RNG
+        else:
+            self.logger.error("Unsuccessfully solved. Unable to pass the check.")
+            return None
+    
+    def getSeed(self, smallseed:bool=False, keylen:int=624, lower:int=None):
+        """
+        Guess the seed with the state.
+        """
+        # if you want to get a smallseed, let smallseed=True && keylen=624
+        if smallseed and (keylen != 624 or lower is not None):
+            self.logger.error("If you really need a small seed, implement your own logic to find one or just let keylen=624 and lower=None.")
+            return
+        if self.state == []:
+            self.logger.error("Please pass the randomPredict first.")
+            return
+        N = 624
+        uint32_mask = 1 << 32
+        state = list(self.state)
+        assert len(state) == N
+        if state[0] != 0x80000000:
+            self.logger.error("The state is invalid.")
+            return
+        
+        step1_rnd = max(624, keylen)
+        state[0] = state[N-1]
+        i = (1 + step1_rnd) % 623
+        for k in range(N - 1):
+            i = i - 1
+            if i <= 0:
+                i += 623
+            state[i] = ((state[i] + i) ^ ((state[i-1] ^ (state[i-1] >> 30)) * 1566083941)) % uint32_mask
+            state[0] = state[N-1]
+        origin_state = [0 for _ in range(N)]
+        origin_state[0] = 19650218
+        for i in range(1, N):
+            origin_state[i] = (1812433253 * (origin_state[i-1] ^ (origin_state[i-1] >> 30)) + i) % uint32_mask
+        origin = origin_state[:]
+        # start to find the key
+        key = [0 for i in range(keylen)]
+        if lower == None:
+            for i in range(keylen - 623):
+                key[i] = 1+i
+        else:
+            if lower.bit_length() > 32 * (keylen - 623):
+                self.logger.error("Too much fixed bits")
+                return
+            for i in range(keylen - 623):
+                key[i] = (lower+i) % uint32_mask
+                lower >>= 32
+        i, j = 1, 0
+        for _ in range(step1_rnd - 623):
+            origin_state[i] = ((origin_state[i] ^ ((origin_state[i-1] ^ (origin_state[i-1] >> 30)) * 0x19660d)) + key[j]) % uint32_mask
+            i += 1
+            j += 1
+            if i >= 624:
+                origin_state[0] = origin_state[623]
+                i = 1
+        for _ in range(N - 1):
+            x = (origin_state[i] ^ ((origin_state[i-1] ^ (origin_state[i-1] >> 30)) * 1664525)) % uint32_mask
+            key[j] = (state[i] - x) % uint32_mask
+            origin_state[i] = ((origin_state[i] ^ ((origin_state[i-1] ^ (origin_state[i-1] >> 30)) * 1664525)) + key[j]) % uint32_mask
+            i += 1
+            if i == N:
+                origin_state[0] = origin_state[N-1]
+                i = 1
+            j += 1
+        
+        if smallseed:
+            T = self._findMinPeriod(key[2:-1])
+            smallkey = key[T:T+2]+key[2:T] if T>1 else key[2:3]
+            x1 = ((origin[1] ^ ((origin[0] ^ (origin[0] >> 30)) * 1664525)) + smallkey[0]) % uint32_mask
+            x2 = ((origin[2] ^ ((x1 ^ (x1 >> 30)) * 1664525)) + smallkey[1%T]) % uint32_mask
+            x3 = ((x1 ^ ((origin_state[keylen-1] ^ (origin_state[keylen-1] >> 30)) * 1664525)) + smallkey[(keylen-1)%T]) % uint32_mask
+            if x2==state[2] and x3==state[1]:
+                self.logger.info(f"Find a small seed with key period: {T}.")
+                return self._mySeed(smallkey)
+            else:
+                self.logger.error("No small seed found.")
+                return 
+        return self._mySeed(key)
+
+def check_normal():
+    # normal check
+    crack = CrackRandom()
+    seed = os.urandom(16)
+    target = random.Random(seed)
+
+    M = 25000
+    length = 12
+    times = M//length
+    for i in range(times):
+        crack.uploadValues(length,target.getrandbits(length))
+    RNG = crack.randomPredict()
+    
+    assert RNG.getstate() == random.Random(seed).getstate()
+
+def check_seed():
+    # check with small seed
+    crack = CrackRandom()
+    seed = random.SystemRandom().randint(0, 1<<128)
+    crack.state = random.Random(seed).getstate()[1][:-1]
+    myseed = crack.getSeed(smallseed=True)
+    assert seed == myseed
+
+    # check with different keylen and lower
+    crack = CrackRandom()
+    seed = random.SystemRandom().randint(0, 1<<128)
+    crack.state = random.Random(seed).getstate()[1][:-1]
+    lower = 0x1145141919810
+    myseed = crack.getSeed(keylen=700, lower=lower)
+    assert myseed & 0xFFFFFFFFFFFFFFFF == lower
+    assert random.Random(seed).getstate() == random.Random(myseed).getstate()
+
+def check_randomlength():
+    # check with random bitlength
+    crack = CrackRandom()
+    seed = os.urandom(16)
+    target = random.Random(seed)
+
+    leakbits = 0
+    M = 30000 # Need more to make the rank of the matrix = 19937, should fail if M = 19968.
+    while leakbits < M:
+        length = random.SystemRandom().randint(10,20)
+        leakbits += length
+        crack.uploadValues(length,target.getrandbits(length))
+    RNG = crack.randomPredict()
+    
+    assert RNG.getstate() == random.Random(seed).getstate()
+
+def check_randomlengthwithunknown():
+    # check with random bitlength and unknown bits
+    crack = CrackRandom()
+    seed = os.urandom(16)
+    target = random.Random(seed)
+
+    leakbits = 0
+    M = 36000 # Need more to make the rank of the matrix = 19937, should fail if M = 19968.
+    while leakbits < M:
+        length = random.SystemRandom().randint(10,20)
+        known = random.SystemRandom().random() > 0.005
+        if known:
+            leakbits += length
+        else:
+            target.getrandbits(length)
+        crack.uploadValues(length,target.getrandbits(length) if known else 0, known)
+    RNG = crack.randomPredict()
+    
+    assert RNG.getstate() == random.Random(seed).getstate()
+
+def check_randomlengthwithunknownandseed():
+    # check with random bitlength and unknown bits and seed
+    crack = CrackRandom()
+    seed = os.urandom(16)
+    target = random.Random(seed)
+
+    leakbits = 0
+    M = 36000 # Need more to make the rank of the matrix = 19937, should fail if M = 19968.
+    while leakbits < M:
+        length = random.SystemRandom().randint(10,20)
+        known = random.SystemRandom().random() > 0.005
+        if known:
+            leakbits += length
+        else:
+            target.getrandbits(length)
+        crack.uploadValues(length,target.getrandbits(length) if known else 0, known)
+    RNG = crack.randomPredict()
+    myseed = crack.getSeed(smallseed=True)
+
+    assert int.from_bytes(seed + hashlib.sha512(seed).digest(), "big") == myseed
+
     
 if __name__ == "__main__":
-    crack = CrackRandom()
-    length = 8
-    times = 19968//length
-    for i in range(times):
-        crack.uploadValues(length,getrandbits(length))
-    RNG = crack.randomPredict()
-    for i in range(times):
-        RNG.getrandbits(length)
-    print(RNG.getrandbits(length),getrandbits(length))
-
-    # crack = CrackRandom("DEBUG")
-    # bitlenths = [1,44,63,22]*(19968//64)
-    # assert 19968==sum(bitlenths[::2])
-    # for i in range(len(bitlenths)):
-    #     if i%2==0:
-    #         crack.uploadValues(bitlenths[i],getrandbits(bitlenths[i]),True)
-    #     else:
-    #         getrandbits(bitlenths[i])   #left unknown
-    #         crack.uploadValues(bitlenths[i],0,False)
-    # RNG = crack.randomPredict()
-
-    # crack = CrackRandom()
-    # length = 12
-    # times = 19968//length+500   # need more to make sure the matrix's rank is 19937
-    # for i in range(times):
-    #     crack.uploadValues(length,getrandbits(length))
-    # RNG = crack.randomPredict()
-    # for i in range(times):
-    #     RNG.getrandbits(length)
-    # print(RNG.getrandbits(length),getrandbits(length))
+    check_normal()
+    # check_seed()
+    # check_randomlength()
+    # check_randomlengthwithunknown()
+    # check_randomlengthwithunknownandseed()
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WuYan's code for cracking MT19937
+# WuYan's code for cracking MT19937 (2.0 Version)
 
-不仅仅能实现Python官方库的randcrack，还打破了每次泄露数据必须是32位的限制，适配**隔任意位**泄露任意位随机数，只要泄露数据超过19968比特就可以构建矩阵。
+不仅仅能实现Python官方库的randcrack，还打破了每次泄露数据必须是32位的限制，适配**隔任意位**泄露**任意位**随机数，只要泄露数据就可以构建矩阵并找到能生成此类随机数的**种子**(泄露数据足够多可保证求解存在**唯一解**)。
 
 破解过程仅需五分钟左右 ~~（比我刚入门时用的要跑一个多小时的脚本不知道快了多少）~~
 
@@ -12,4 +12,10 @@ Sage!S4ge!5493!Without sage we can't do anything!
 
 ## Specific
 
-具体使用方法详见我的博客 <http://www.wuy4n.com/2025/03/26/PYRandCrack/>
+具体使用方法详见我的博客 <http://www.wuy4n.com/2025/09/17/PYRandCrack2/>
+
+## Others
+
+2.0版本新增
++ 支持根据随机数生成器状态直接求解种子
++ 更新多个check函数，便于测试与用户理解
